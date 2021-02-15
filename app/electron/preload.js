f3483ccd5891eab8e03dab245a71ad552e345d25
ADIthaker <adityathaker28@gmail.com>
2021-02-15 14:52:21 +0530

    example for ipc messaging
    event listeners registered on react components call api functions in preload.js file those functions call ipcRenderer events
    , these events then talk to the ipcMain on a channel
    NOTE : two functions are to be called one asking for data and one that specifies what to do with said data after its
    returned, so export accordingly
    each ipcMain.on takes the data and replies on a specific channel, dont forget to fucntion to handle that as well also
    provide this function to frontend as a getter of the data after main has finished processing

--- app/electron/preload.js
+++ app/electron/preload.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const i18nextBackend = require("i18next-electron-fs-backend");
 const Store = require("secure-electron-store").default;
 const ContextMenu = require("secure-electron-context-menu").default;
-// const api = require("./renderEvents");
+//  const { sendHello,getCollege } = require("./renderEvents");
 // Create the electron store to be made available in the renderer process
 const store = new Store();
 
@@ -13,4 +13,6 @@ contextBridge.exposeInMainWorld("api", {
   i18nextElectronBackend: i18nextBackend.preloadBindings(ipcRenderer),
   store: store.preloadBindings(ipcRenderer, fs),
   contextMenu: ContextMenu.preloadBindings(ipcRenderer),
+  //  send: name => sendHello(name),
+  //  get: ()=> getCollege(),
 });
