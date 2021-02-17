ab2bc47e22a866f1bb96b121499e89deb7822b1f
ADIthaker <adityathaker28@gmail.com>
2021-02-17 10:19:24 +0530

    installation and rearranging components
    navbar has been moved to components to make it more semantic

--- app/src/pages/Dashboard/Dashboard.js
+++ app/src/pages/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./Navbar/Navbar";
+import Navbar from "../../components/Navbar/Navbar";
 import AddProject from "./AddProject";
 import RecentProjects from "./RecentProjects";
 function Dashboard() {
