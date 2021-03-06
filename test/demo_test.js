51cf42c83c070f20cbe0751a26222aedad0df570
Yashodhan Joshi <54112038+YJDoc2@users.noreply.github.com>
2021-03-06 14:57:18 +0530

    Add testing demo

---
+++ test/demo_test.js
@@ -0,0 +1,10 @@
+const { expect } = require("chai");
+
+describe("Demo Test Group", () => {
+  it("Should show how testing is done", () => {
+    const demoReturn = { key1: "val1" }; // Call actual Function instead
+
+    expect(demoReturn).to.have.property("key1");
+    expect(demoReturn.key1).to.be.equal("val1");
+  });
+});
