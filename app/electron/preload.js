976f42d13649ec21d1dec7dd9d032307ab608657
ADIthaker <adityathaker28@gmail.com>
2021-03-22 21:33:05 +0530

    added changes to event listening setup from Ipc renderer patch pull request --missed a file

--- app/electron/preload.js
+++ app/electron/preload.js
@@ -4,8 +4,7 @@ const i18nextBackend = require("i18next-electron-fs-backend");
 const Store = require("secure-electron-store").default;
 const ContextMenu = require("secure-electron-context-menu").default;
 const log = require("electron-log");
-
-//  const { sendHello,getCollege } = require("./renderEvents");
+const events = require("./events");
 // Create the electron store to be made available in the renderer process
 const store = new Store();
 
@@ -15,8 +14,7 @@ contextBridge.exposeInMainWorld("api", {
   i18nextElectronBackend: i18nextBackend.preloadBindings(ipcRenderer),
   store: store.preloadBindings(ipcRenderer, fs),
   contextMenu: ContextMenu.preloadBindings(ipcRenderer),
-  //  send: name => sendHello(name),
-  //  get: ()=> getCollege(),
+  api: events.preloadBindings(ipcRenderer, fs),
 });
 // Logging
 window.log = log.functions;
