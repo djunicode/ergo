37b1fa268b2a5a495205d46d19807f0e8bce865a
Ridmod <hridaymodi@hotmail.com>
2021-03-24 12:52:58 +0530

    updated code for code edittor launch

--- app/electron/launcher.js
+++ app/electron/launcher.js
@@ -1,31 +1,33 @@
 const { exec } = require("child_process");
 const log = require("electron-log");
 
-function launchCodeEdiitor(language, path) {
-  if (language === "Java") {
+function launchCodeEdiitor(preferredEdittor, path) {
+  if (preferredEdittor === "Eclipse") {
     exec(`eclipse "${path}"`, (error) => {
       if (error) {
         log.error(error);
       }
     });
-  } else if (language === "C") {
-    exec(`notepad "${path}"`, (error) => {
+  } else if (preferredEdittor === "Atom") {
+    exec(`atom "${path}"`, (error) => {
       if (error) {
         log.error(error);
       }
     });
-  } else if (language === "HTML" || "CSS") {
-    exec(`atom "${path}"`, (error) => {
+  } else if (preferredEdittor === "Pycharm") {
+    exec(`pycharm64.exe "${path}"`, (error) => {
       if (error) {
-        try {
-          exec(`code "${path}"`);
-        } finally {
-          log.error(error);
-        }
+        log.error(error);
+      }
+    });
+  } else if (preferredEdittor === "VSCode") {
+    exec(`code "${path}"`, (error) => {
+      if (error) {
+        log.error(error);
       }
     });
   } else {
-    exec(`code "${path}"`, (error) => {
+    exec(`notepad "${path}"`, (error) => {
       if (error) {
         log.error(error);
       }
@@ -36,8 +38,11 @@ function launchCodeEdiitor(language, path) {
 function launchFileManager(path) {
   if (process.platform === "linux") {
     exec(`xdg-open "${path}"`);
-  } else;
-  exec(`explorer "${path}"`);
+  }
+  if (process.platform === "win") {
+    exec(`explorer "${path}"`);
+  }
 }
 launchFileManager("add path here");
 launchCodeEdiitor("add language", "C:\\path\\to\\folder\\here");
+module.exports = launchCodeEdiitor;
