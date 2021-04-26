10b4eb4c95d7beb3202e564b7a169b032243bede
ADIthaker <adityathaker28@gmail.com>
2021-04-26 13:26:45 +0530

    promisified remaining launcher functions

--- app/electron/events/launcher.js
+++ app/electron/events/launcher.js
@@ -1,27 +1,41 @@
 /* eslint-disable consistent-return */
 const { exec } = require("child_process");
-const log = require("electron-log");
 
-exports.launchCodeEdiitor = function (preferredEdittor, path) {
+exports.launchCodeEdiitor = (preferredEdittor, path) => {
   if (preferredEdittor === "Eclipse") {
-    exec(`eclipse "${path}"`, (error) => {
-      if (error) {
-        log.error(error);
-      }
+    return new Promise((resolve, reject) => {
+      exec(`eclipse "${path}"`, (error) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve({ success: true });
+        }
+      });
     });
-  } else if (preferredEdittor === "Atom") {
-    exec(`atom "${path}"`, (error) => {
-      if (error) {
-        log.error(error);
-      }
+  }
+  if (preferredEdittor === "Atom") {
+    return new Promise((resolve, reject) => {
+      exec(`atom "${path}"`, (error) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve({ success: true });
+        }
+      });
     });
-  } else if (preferredEdittor === "Pycharm") {
-    exec(`pycharm64.exe "${path}"`, (error) => {
-      if (error) {
-        log.error(error);
-      }
+  }
+  if (preferredEdittor === "Pycharm") {
+    return new Promise((resolve, reject) => {
+      exec(`pycharm64.exe "${path}"`, (error) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve({ success: true });
+        }
+      });
     });
-  } else if (preferredEdittor === "VSCode") {
+  }
+  if (preferredEdittor === "VSCode") {
     return new Promise((resolve, reject) => {
       exec(`code "${path}"`, (error) => {
         if (error) {
@@ -31,16 +45,19 @@ exports.launchCodeEdiitor = function (preferredEdittor, path) {
         }
       });
     });
-  } else {
+  }
+  return new Promise((resolve, reject) => {
     exec(`notepad "${path}"`, (error) => {
       if (error) {
-        log.error(error);
+        reject(error);
+      } else {
+        resolve({ success: true });
       }
     });
-  }
+  });
 };
 
-exports.launchFileManager = function (path) {
+exports.launchFileManager = (path) => {
   if (process.platform === "linux") {
     return new Promise((resolve, reject) => {
       exec(`xdg-open "${path}"`, (error) => {
